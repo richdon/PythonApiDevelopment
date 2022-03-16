0c8a0fb09a3649195988fec3fc978bdf885d404d
Richard Donald <richard_donald92@outlook.com>
2022-03-15 23:03:33 -0400

    create database session

---
+++ app/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+f = open('files/pw.txt', 'r')
+pw = f.read()
+f.close()
+
+SQLAlCHEMY_DATABASE_URL = f'postgresql://postgres:{pw}@localhost/fast-api-social-media-app'
+
+engine = create_engine(SQLAlCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Tables will extend this class
+Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
