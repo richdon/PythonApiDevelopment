8654f6d99a14bbf766fa7ac21581f9dede423c3c
Richard Donald <richard_donald92@outlook.com>
2022-03-16 00:06:59 -0400

    Add response post structure

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
+from typing import List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 from psycopg2.extras import RealDictCursor
 from psycopg2 import OperationalError
-from database import engine, get_db
-import models
-import schemas
+from .database import engine, get_db
+from . import models, schemas
 import psycopg2
 import time
 
@@ -43,8 +43,14 @@ def root():
     return {"message": "Welcome to my API !!!!! cool"}
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
+@app.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -57,10 +63,10 @@ def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
     db.commit()
     # store the data added by postgres back to the original query variable
     db.refresh(new_post_query)
-    return {"data": new_post_query}
+    return new_post_query
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
     # post = cursor.fetchone()
@@ -70,7 +76,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id {id} was not found')
-    return {"post_detail": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -90,8 +96,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s , content = %s, published = %s WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
@@ -107,4 +113,5 @@ def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)):
                             detail=f'post with id {id} was not found')
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    post = post_query.first()
+    return post
