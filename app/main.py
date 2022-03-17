1fc7f38f9b3915f8c8df21064a171b012807eb75
Richard Donald <richard_donald92@outlook.com>
2022-03-16 22:55:23 -0400

    remove the sql being used before ORM

--- app/main.py
+++ app/main.py
@@ -16,30 +16,12 @@ f = open('files/pw.txt', 'r')
 pw = f.read()
 f.close()
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fast-api-social-media-app', user='postgres',
-                                password=pw, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Database connection was successfully created')
-        break
-
-    except Exception as e:
-        if isinstance(e, OperationalError):
-            if 'password authentication failed' in e.args[0]:
-                print("Error: Wrong password entered for database. Exiting...")
-                exit(code=1)
-        print("Connection to database failed. Retrying connection in 2 seconds...")
-        print(f"Error: {e}")
-        time.sleep(2)
-
 
 # Path (or Route) operation
 @app.get("/")  # root path
 # Path operation function
 def root():
-    # whatever is returned is what gets sent back to the user and Fast A
-    # PI converts it to JSON
+    # whatever is returned is what gets sent back to the user and Fast API converts it to JSON
     return {"message": "Welcome to my API !!!!! cool"}
 
 
@@ -51,10 +33,6 @@ def get_posts(db: Session = Depends(get_db)):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
     # build the query
     new_post_query = models.Post(**post.dict())
     # add the query to postgres
@@ -68,9 +46,6 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 @app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    # post = cursor.fetchone()
-    #
     query = db.query(models.Post).filter(models.Post.id == id)
     post = query.first()
     if not post:
@@ -81,12 +56,6 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
-    # deleted_post = cursor.fetchone()
-    # if not deleted_post:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-    #                         detail=f'post with id {id} does not exist')
-    # conn.commit()
     post = db.query(models.Post).filter(models.Post.id == id)
     if not post.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -98,14 +67,6 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 @app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s , content = %s, published = %s WHERE id = %s RETURNING *""",
-    #                (post.title, post.content, post.published, str(id)))
-    # updated_post = cursor.fetchone()
-    # if not updated_post:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-    #                         detail=f'post with id {id} was not found')
-    # conn.commit()
-
     post_query = db.query(models.Post).filter(models.Post.id == id)
     my_post = post_query.first()
     if not my_post:
