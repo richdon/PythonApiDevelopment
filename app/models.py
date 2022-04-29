240671ab7d08079a07f504befb91061db74d6ba0
Richard Donald <richard_donald92@outlook.com>
2022-04-28 20:08:26 -0400

    update and delete only logged in user's posts

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import null, text
 from .database import Base
@@ -12,10 +12,12 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
 
 
 class User(Base):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
--- routers/post.py
+++ routers/post.py
@@ -20,9 +20,9 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemes.Post)
 def create_posts(post: schemes.PostCreate, db: Session = Depends(get_db),
                  current_user: schemes.UserBase = Depends(oauth2.get_current_user)):
-    print(current_user.email)
+    print(current_user.id)
     # build the query
-    new_post_query = models.Post(**post.dict())
+    new_post_query = models.Post(owner_id=current_user.id, **post.dict())
     # add the query to postgres
     db.add(new_post_query)
     # commit the staged query
@@ -44,11 +44,16 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if not post.first():
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id {id} does not exist')
-    post.delete(synchronize_session=False)
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -61,6 +66,8 @@ def update_post(id: int, post: schemes.PostCreate, db: Session = Depends(get_db)
     if not my_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id {id} was not found')
+    if my_post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
     post = post_query.first()
